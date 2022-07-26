8c852544708309b6e8c964157dec02f22abadae2
Anny Patino <103137175+apatino16@users.noreply.github.com>
2022-07-25 19:47:24 -0700

    Update mealMaker.js
    Create an object called menu

--- mealMaker.js
+++ mealMaker.js
@@ -1,1 +1,14 @@
+// Key Concepts: JavaScript Objects
+  // Understanding how Objects work on JavaScript
+  // How to create Objects 
+  // How to access Objects' Properties 
+  // Bracket Notation
+  // Property Assignments
+  // Use of Getter and Setters Methods
 
+// Goal: Add functionability to a website by creating a function that allows a restaurant to 
+// set a meal and price each morning for their Special of the Day.
+
+//Create a Menu Object
+
+const menu = { };
