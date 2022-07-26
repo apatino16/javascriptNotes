9528df7845514682f39168485c9e3d6a84502a41
Anny Patino <103137175+apatino16@users.noreply.github.com>
2022-07-26 16:52:48 -0700

    Update kelvinWeather.js
    convert Kelvin to Celsius, then to Fahrenheit

--- kelvinWeather.js
+++ kelvinWeather.js
@@ -1,1 +1,32 @@
+// Key Concepts
+  // Comments
+  // Constant variables
+  // Math.floor 
+  // Log 
+
+// Goal:convert Kelvin to Celsius, then to Fahrenheit
+
+// The forecast today is 293 Kelvin. The value will stay constant. 
+const kelvin = 293;
+
+//To convert celsius to kelvin you have to subtract 273. 
+const celsius = kelvin - 273;
+
+//Assigning Farenheit
+let fahrenheit = celsius * (9/5) + 32;
+
+//Round Farenheit temperature
+fahrenheit = Math.floor(fahrenheit);
+
+//Interpolating the string to log the temperature in Farenheit
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+//Converting Celsius to the Newton scale
+let newton = celsius * (33/100);
+
+//Rounding down the Newton temperature
+newton = Math.floor(newton);
+
+//Interpolating the string to log the temperature in Newton
+console.log(`The temperature is ${newton} degrees Newton.`);
 
