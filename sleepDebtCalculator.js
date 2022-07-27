14f2662f5d02cff5e96e272e35f9144fe0a279c5
Anny Patino <103137175+apatino16@users.noreply.github.com>
2022-07-26 19:29:45 -0700

    Update sleepDebtCalculator.js

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,1 +1,7 @@
+// Key Concepts:
+
+// Goal:
+  // Determine the actual and ideal hours of sleep for each night of last week.
+  // Calculate, in hours, how far you are from your weekly sleep goal.
+
 
